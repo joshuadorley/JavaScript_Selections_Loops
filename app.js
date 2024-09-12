426f3aa9e7c0857e985e2d3c34c667f48463999a
joshuadorley <joshuaboateng33@gmail.com>
2024-09-12 18:29:40 -0400

    Finished

--- app.js
+++ app.js
@@ -3,5 +3,79 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+for (let i = 0; i < 100; i ++) {
+    if (i % 2 == 0) {
+        continue;    
+    } {
+        console.log(i)
+    }
+}
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+for (let i = 1; i < 100; i++) {
+    console.log(i);
+
+    if (1 % 3 == 0 && i % 5 == 0) {
+        console.log("FIZZBUZZ");
+    } else {
+        if (i % 3 == 0) {
+            console.log("FIZZ")
+        }
+
+        if (i % 5 == 0) {
+            console.log("BUZZ");
+        }
+    }
+}
+
+let i = 1;
+while (i <= 100) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+let x = 1;
+do {
+    if (x % 2 !== 0) {
+        console.log(x);
+    }
+    x++;
+} while (x <= 100);
+
+let value = Math.round(Math.random() * 500);
+let n = Math.round(Math.random() * (500 - 100) + 100);
+
+let found = false;
+
+for (let i = 0; i <= n; i++) {
+    if (i === value) {
+        console.log("Found value!");
+        found = true;
+        break;
+    }
+}
+
+if (!found) {
+    console.log("Did not find value");
+}
+
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let start = Math.round(Math.random() * (10 - 1) + 1);
+let end = Math.round(Math.random() * (1000 - 100) + 100);
+
+for (let i = start; i <= end; i++) {
+    if (i % fizzDivisor === 0 && i % buzzDivisor === 0) {
+        console.log('FIZZBUZZ');
+    } else if (i % fizzDivisor === 0) {
+        console.log('FIZZ');
+    } else if (i % buzzDivisor === 0) {
+        console.log('BUZZ');
+    } else {
+        console.log(i);
+    }
+}
